11d0e65b71f9d0b1b5f44d84ec80ed54710e4b56
magrils <magrils@post.bgu.ac.il>
2020-12-28 20:35:38 +0200

    changed server main function behavior

--- src/Server.py
+++ src/Server.py
@@ -1,23 +1,57 @@
 import socket
+import select
+import time
+import scapy.all
+import struct
+
 def Main(): 
-    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      # UDP socket
+    start_server()
 
-    #udp_host = socket.gethostname()		        # Host IP
-    udp_host = '127.0.0.1'		        # Host IP
-    udp_port = 13117			                # specified port to connect
-
-    #print type(sock) ============> 'type' can be used to see type 
-                    # of any variable ('sock' here)
-
-    sock.bind((udp_host,udp_port))
-
+def start_server():
+    udp_sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)  # UDP socket
+    # udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #enable broadcast ??
+    udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) #enable broadcast ??
     
-    print ("host ip:",udp_host," port:",udp_port,"\n")
+    
+    #udp_host = socket.gethostname()		    
+    HOST = scapy.all.get_if_addr('eth1')		                                # Host IP
+    PORT = 13000			                                # specified PORT to connect
+    DEST_PORT = 13117                                       # destenation PORT for broadcast
+    BROADCAST_ADDR = '172.1.255.255'     # ='127.1.0.255'        # broadcast IP
+    offer_str = "test offer!!"
+    
+    # udp_sock.bind((HOST,PORT))
+    print ("Server started, listening on IP address ",HOST)
+    
+    # When we want to send the next periodic-ping-message out
+    nextCastTime = time.time()
+ 
+    # for now using 'select()' for multiplex the two tasks, consider changing to two threads
     while True:
-        print ("Waiting for client...")
-        data,addr = sock.recvfrom(1024)	        #receive data from client
-        print ("Received Messages:",data," from",addr)
+        secondsUntilNextCast = nextCastTime - time.time()
+        if (secondsUntilNextCast < 0):    
+            secondsUntilNextCast = 0
 
+        # select() won't return until 'udp_sock' has some data
+        # ready-for-read, OR until secondsUntilNextPing seconds 
+        # have passed, whichever comes first
+        inReady, _ , _ = select.select([udp_sock], [], [], secondsUntilNextCast)
+
+        if (udp_sock in inReady):
+            # There's an incoming UDP packet ready to receive!
+            print(udp_sock.recvfrom(100))
+            gather_client()
+
+        now = time.time()
+        if (now >= nextCastTime):
+            # Time to send out the next Cast!
+            print ("Sending out scheduled Cast at time ", now)
+            udp_sock.sendto(offer_str.encode(), (BROADCAST_ADDR, DEST_PORT))
+            nextCastTime = now + 1.0   # do it again in another second
+
+
+def gather_client():
+    print("method gather_client not yet inplemented")
 
 if __name__ == '__main__': 
 	Main() 
---
+++ src/testUDPclient.py
@@ -0,0 +1,25 @@
+
+import socket
+import scapy.all
+import struct
+
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)  # UDP socket
+# sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #enable broadcast ??
+# sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) #enable broadcast ??
+
+udp_host = scapy.all.get_if_addr('eth1')               # The server's hostname or IP address
+udp_port = 13117			        # specified port to connect
+
+msg = "Hello Python! from sharon & gil"
+print ("UDP host IP:", udp_host)
+print ("UDP host Port:", udp_port)
+
+# sock.sendto(msg.encode(),(udp_host,udp_port))		# Sending message to UDP server
+
+sock.bind(("",udp_port))
+
+while True:
+	print ("Waiting for server...")
+	message,addr = sock.recvfrom(1024)	        #receive data from client
+	print ("Received Messages:",message," from",addr)
